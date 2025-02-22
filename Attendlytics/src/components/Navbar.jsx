1800eefa10a2ceeb28cc281914cc26812c1b0217
Abhijnan S <114734468+Abhijnan18@users.noreply.github.com>
2025-02-22 15:40:13 +0530

    working

--- Attendlytics/src/components/Navbar.jsx
+++ Attendlytics/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
 
   return (
     <nav className="bg-[#050505]">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-start mx-auto p-6">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="text-2xl text-gray-100">
             <span className="font-bold">A</span>
@@ -13,40 +13,31 @@ export default function Navbar() {
           </span>
         </a>
 
-        <div className="flex items-center md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse">
+        <div className="flex items-center md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse ml-auto">
           <button
             onClick={() => setIsMenuOpen(!isMenuOpen)}
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden hover:underline "
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden hover:underline"
             aria-expanded={isMenuOpen}
           >
             <span className="sr-only">Open main menu</span>
-            <p className="text-white">Menu</p>
+            <p className="text-white text-base">Menu</p>
           </button>
         </div>
 
         <div
-          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+          className={`items-center md:pl-[1.5rem] justify-between w-full md:flex md:w-auto md:order-1 ${
             isMenuOpen
-              ? "fixed inset-0 z-40 bg-[#050505] top-20 md:top-16 md:relative md:bg-transparent"
+              ? "fixed inset-0 z-40 bg-[#050505] top-[4rem] md:top-16 md:relative md:bg-transparent"
               : "hidden"
           }`}
           id="navbar-language"
         >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-0 md:flex-row md:space-x-8 md:mt-0">
+          <ul className="flex flex-col font-medium p-6 md:p-0 mt-0 md:flex-row md:space-x-8 md:mt-0">
             <li>
               <a
                 href="#"
-                className="block py-4 px-3 text-2xl md:text-base text-white md:text-gray-400 md:hover:text-white rounded md:bg-transparent md:p-0"
-                onClick={() => setIsMenuOpen(false)}
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-4 px-3 text-2xl md:text-base text-white md:text-gray-400 md:hover:text-white rounded hover:bg-gray-900 md:hover:bg-transparent md:p-0"
+                className="block pl-0 py-[1rem] px-3 text-[1.75rem] font-normal leading-6 md:font-light md:text-base text-white md:text-gray-400 md:hover:text-white rounded hover:bg-gray-900 md:hover:bg-transparent md:p-0"
                 onClick={() => setIsMenuOpen(false)}
               >
                 About
@@ -55,7 +46,7 @@ export default function Navbar() {
             <li>
               <a
                 href="#"
-                className="block py-4 px-3 text-2xl md:text-base text-white md:text-gray-400 md:hover:text-white rounded hover:bg-gray-900 md:hover:bg-transparent md:p-0"
+                className="block pl-0 py-[1rem] px-3 text-[1.75rem] font-normal leading-6 md:font-light md:text-base text-white md:text-gray-400 md:hover:text-white rounded hover:bg-gray-900 md:hover:bg-transparent md:p-0"
                 onClick={() => setIsMenuOpen(false)}
               >
                 Services
@@ -64,7 +55,7 @@ export default function Navbar() {
             <li>
               <a
                 href="#"
-                className="block py-4 px-3 text-2xl md:text-base text-white md:text-gray-400 md:hover:text-white rounded hover:bg-gray-900 md:hover:bg-transparent md:p-0"
+                className="block pl-0 py-[1rem] px-3 text-[1.75rem] font-normal leading-6 md:font-light md:text-base text-white md:text-gray-400 md:hover:text-white rounded hover:bg-gray-900 md:hover:bg-transparent md:p-0"
                 onClick={() => setIsMenuOpen(false)}
               >
                 Pricing
@@ -73,7 +64,7 @@ export default function Navbar() {
             <li>
               <a
                 href="#"
-                className="block py-4 px-3 text-2xl md:text-base text-white md:text-gray-400 md:hover:text-white rounded hover:bg-gray-900 md:hover:bg-transparent md:p-0"
+                className="block pl-0 py-[1rem] px-3 text-[1.75rem] font-normal leading-6 md:font-light md:text-base text-white md:text-gray-400 md:hover:text-white rounded hover:bg-gray-900 md:hover:bg-transparent md:p-0"
                 onClick={() => setIsMenuOpen(false)}
               >
                 Contact
